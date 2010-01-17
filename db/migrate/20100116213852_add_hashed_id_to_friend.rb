899e34beda16702945b5177eeaf45637e6601ab3
Luke Wendling <luke@lukewendling.com>
2010-01-16 19:06:07 -0600

    fix broken migration

--- db/migrate/20100116213852_add_hashed_id_to_friend.rb
+++ db/migrate/20100116213852_add_hashed_id_to_friend.rb
@@ -3,13 +3,6 @@ class AddHashedIdToFriend < ActiveRecord::Migration
     add_column :friends, :hashed_id, :string
     
     add_index :friends, :hashed_id, :unique => true
-    
-    say 'Updating friends ...'
-    Friend.reset_column_information
-    Friend.all.each do |f|
-      f.hashed_id = Digest::SHA1.hexdigest([Time.now, rand].join)
-      f.save!
-    end
   end
 
   def self.down
