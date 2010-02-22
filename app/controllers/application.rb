7859c88e08eedb3213ea04aa517dd0ff9dea6da9
Luke Wendling <luke@lukewendling.com>
2010-02-21 21:02:25 -0600

    add jqtouch files. add basic mobile layout

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,16 +12,31 @@ class ApplicationController < ActionController::Base
   filter_parameter_logging :password, :password_confirmation
   
   before_filter :login_required
-  
+  before_filter :prepare_for_mobile
+    
   #rescue twitter gem auth errors globally
   rescue_from Twitter::Unauthorized, :with => :twitter_unauthorized
   
   def boom
     raise 'boom'
   end
+
+  def mobile_device?  
+    if session[:mobile_param]  
+      session[:mobile_param] == "1"  
+    else  
+      request.user_agent =~ /Mobile|webOS/  
+    end  
+  end  
+  helper_method :mobile_device?
   
   private
     def twitter_unauthorized(exception)
       redirect_to new_authorization_url
     end
+
+   def prepare_for_mobile  
+     session[:mobile_param] = params[:mobile] if params[:mobile]
+     request.format = :mobile if mobile_device?
+   end 
 end
